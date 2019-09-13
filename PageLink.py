58180ff96d63e4c8660cbae0e33f46792ba6144f
alegemaate <alegemaate@gmail.com>
2019-09-13 19:12:02 -0400

    Output json, external links per node

--- PageLink.py
+++ PageLink.py
@@ -1,9 +1,22 @@
-'''Named tuple creation'''
+'''Named tuple creation and json lib'''
 from typing import NamedTuple
 
-class PageLink(NamedTuple):
+class PageLink:
   ''' Strucutre for page link'''
+  url = None
+  internal = None
+  external = None
+  status = None
+  depth = None
+
+  def __init__(self, url, internal, external, status, depth):
+    self.url = url
+    self.internal = internal
+    self.external = external
+    self.status = status
+    self.depth = depth
+
+class DepthLink(NamedTuple):
+  ''' Basic structure with deptha and url'''
   url: str
-  links: set
-  status: int
   depth: int
--- scrape.py
+++ scrape.py
@@ -1,16 +1,18 @@
 """Imports"""
-import requests
 import sys
+import json
+import requests
 from bs4 import BeautifulSoup
 from PageLink import PageLink
 
-def crawl_page(page, depth):
+def crawl_page(page, depth, base_url):
   """Get individual page"""
   # Echo page
   print("Travelling to: ", page)
 
   # Set of next links
-  links = set()
+  internal = set()
+  external = set()
 
   # Make request
   response = requests.get(page)
@@ -30,64 +32,55 @@ def crawl_page(page, depth):
 
       next_link = current_tag['href']
 
-      # Ensure we are going to html page
-      links.add(next_link)
+      # Remove incorrect slashes and check domain
+      next_link = next_link.replace('\\', '/')
+      if base_url not in next_link:
+        # External link
+        if '://' in next_link:
+          external.add(next_link)
+        # Add base url to current
+        else:
+          internal.add(base_url + next_link)
+      else:
+        internal.add(next_link)
 
-  return PageLink(page, links, status, depth + 1)
+  return PageLink(page, list(internal), list(external), status, depth)
 
 
-def crawl(base_url):
+def crawl(base_url, max_depth):
   """Entry point"""
 
-  # Set of links still needing visiting
-  unvisited = {base_url}
+  # Track visited, external and to be visited links
+  unvisited = {(base_url, 0)}
+  visited = []
 
-  # External links
-  external = set()
-
-  # Tracking of links visited and graph
-  visited = set()
-
-  # Link objects
-  link_obj = []
-
+  # Run until stack is done
   while unvisited != set():
     # Pop first one off of set
-    current = unvisited.pop()
+    (url, depth) = unvisited.pop()
 
-    # Remove incorrect slashes and check domain
-    current = current.replace('\\', '/')
-    if base_url not in current:
-      # External link
-      if '://' in current:
-        external.add(current)
-      # Add base url to current
-      else:
-        current = base_url + current
+    # Skip if at max depth
+    if depth > int(max_depth):
+      continue
 
-    # Ensure we do not begin in external site
-    if current not in external and current not in visited:
-      # Add to visited
-      visited.add(current)
-
+    # Ensure we do not begin already visited site
+    if url not in map(lambda v: v.url, visited):
       # Get links from current page
-      page_link = crawl_page(current, 1)
-      link_obj.append(page_link)
+      page_link = crawl_page(url, depth, base_url)
+
+      # Add to visited
+      visited.append(page_link)
 
       # Add to unvisited, only unvisited
-      new_links = page_link.links.difference(visited)
-      unvisited = unvisited.union(new_links)
+      unvisited_links = filter(lambda l: l not in map(lambda v: v.url, visited), page_link.internal)
+      for link in unvisited_links:
+        unvisited.add((link, depth + 1))
 
-  print('Vistited:')
-  print(visited)
-  print('External:')
-  print(external)
-  #print('obj:')
-  #print(link_obj)
+  print(json.dumps([ob.__dict__ for ob in visited]))
 
 
 # Param check
-if len(sys.argv) < 2:
-  print("Please pass a site to crawl")
+if len(sys.argv) < 3:
+  print("Please pass a site to crawl and a depth")
 else:
-  crawl(sys.argv[1])
+  crawl(sys.argv[1], sys.argv[2])
